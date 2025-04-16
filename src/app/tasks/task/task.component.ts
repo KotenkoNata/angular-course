8567766994194d55a9aacbc7420389fdd51ae8e3
KotenkoNata <kotenkonata8@gmail.com>
2025-04-16 16:59:24 +0200

    feat: Time to Practice: Services

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { Task } from "./task.model";
 import { CardComponent } from "../../shared/card/card.component";
 import { DatePipe } from "@angular/common";
+
+import {TasksService} from "../tasks.service";
 
 @Component({
   selector: 'app-task',
@@ -15,10 +17,9 @@ import { DatePipe } from "@angular/common";
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+  private tasksService = inject(TasksService);
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id)
   }
-
 }
