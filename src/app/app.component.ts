933c216127263ecd41970f5b842007f87f910a63
KotenkoNata <kotenkonata8@gmail.com>
2025-04-17 16:08:02 +0200

    feat: Adding a User Input Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component } from '@angular/core';
-import { DUMMY_USERS } from "./dummy-users";
+import { HeaderComponent } from "./header/header.component";
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
-  standalone: false,
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [
+    HeaderComponent,
+    UserInputComponent
+  ]
 })
-export class AppComponent {
-  users = DUMMY_USERS;
-  selectedUserId?: string;
-
-  get selectedUser(){
-    return this.users.find(user => user.id === this.selectedUserId);
-  }
-
-  onSelectUser(id: string){
-    this.selectedUserId = id;
-  }
-}
+export class AppComponent {}
