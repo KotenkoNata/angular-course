8f018506bae4770385ee1fc01feffca7cfd0ee80
KotenkoNata <kotenkonata8@gmail.com>
2025-04-25 16:41:43 +0200

    feat: Making Sense of Signal Effects

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, DestroyRef } from '@angular/core';
+import { Component, OnInit, inject, DestroyRef, signal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -8,10 +8,15 @@ import { Component, OnInit, inject, DestroyRef } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
   private destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+    effect(() => {
+      console.log(this.currentStatus());
+    });
+
+  }
 
   ngOnInit() {
     console.log('ON ngOnInit');
@@ -20,11 +25,11 @@ export class ServerStatusComponent implements OnInit {
       const rnd = Math.random();
 
       if(rnd < 0.5){
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       }else if(rnd < 0.9){
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       }else{
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000)
 
