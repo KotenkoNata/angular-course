9829cc2b3390fe172c7926435c2ae6dd5a9a4c84
KotenkoNata <kotenkonata8@gmail.com>
2025-05-01 16:37:40 +0200

    feat: Using The Alternative Dependency Injection Syntax

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,19 +1,22 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TasksService } from '../tasks.service';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 
 @Component({
-  selector: 'app-tasks-list',
-  standalone: true,
-  templateUrl: './tasks-list.component.html',
-  styleUrl: './tasks-list.component.css',
-  imports: [TaskItemComponent],
+    selector: 'app-tasks-list',
+    standalone: true,
+    templateUrl: './tasks-list.component.html',
+    styleUrl: './tasks-list.component.css',
+    imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
-  tasks = [];
+    private tasksService = inject(TasksService);
+    selectedFilter = signal<string>('all');
+    tasks = this.tasksService.allTasks;
 
-  onChangeTasksFilter(filter: string) {
-    this.selectedFilter.set(filter);
-  }
+
+    onChangeTasksFilter(filter: string) {
+        this.selectedFilter.set(filter);
+    }
 }
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,18 +1,20 @@
 import { Injectable, signal } from "@angular/core";
-import { Task } from "./task.model";
+import { Task, TaskStatus } from "./task.model";
 
 
 @Injectable({
     providedIn: 'root'
 })
 export class TasksService {
-    tasks = signal<Task[]>([]);
+    private tasks = signal<Task[]>([]);
+
+    allTasks = this.tasks.asReadonly();
 
     addTask(taskData: { title: string, description: string }) {
         const newTask = {
             ...taskData,
             id: Math.random().toString(),
-            status: 'OPEN'
+            status: 'OPEN' as TaskStatus,
         };
         this.tasks.update((oldTasks) => [...oldTasks, newTask]);
     }
