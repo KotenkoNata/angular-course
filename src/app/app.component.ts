8c9f56abba157367f6c9f2f03cedadd98af002c7
KotenkoNata <kotenkonata8@gmail.com>
2025-05-01 16:13:51 +0200

    feat: The Starting Project & The Need For A Centralized Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,11 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { AuthComponent } from './auth/auth.component';
-import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
-import { AuthService } from "./auth/auth.service";
-import { AuthDirective } from "./auth/auth.directive";
-import { LogDirective } from "./log.directive";
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent, AuthDirective, LogDirective],
+  imports: [TasksComponent],
 })
-export class AppComponent {
-  private authService = inject(AuthService);
-
-  isAdmin = computed(() => this.authService.activePermission() === 'admin');
-}
+export class AppComponent {}
