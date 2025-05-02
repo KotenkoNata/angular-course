fd6b50d217a54d5ee53b2cf24807d3a8bf42411b
KotenkoNata <kotenkonata8@gmail.com>
2025-05-02 16:44:59 +0200

    feat: Using Custom DI Tokens & Providers

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,35 +1,40 @@
-import { Component, computed, inject, signal } from '@angular/core';
-import { TasksService } from '../tasks.service';
+import { Component, computed, inject, signal } from "@angular/core";
 
-import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskItemComponent } from "./task-item/task-item.component";
+import { TasksServiceToken } from "../../../main";
 
 @Component({
-    selector: 'app-tasks-list',
-    standalone: true,
-    templateUrl: './tasks-list.component.html',
-    styleUrl: './tasks-list.component.css',
-    imports: [TaskItemComponent],
+  selector: "app-tasks-list",
+  standalone: true,
+  templateUrl: "./tasks-list.component.html",
+  styleUrl: "./tasks-list.component.css",
+  imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-    private tasksService = inject(TasksService);
-    private selectedFilter = signal<string>('all');
-    tasks = computed(()=>{
-        switch(this.selectedFilter()){
-            case 'all':
-                return this.tasksService.allTasks();
-            case 'open':
-                return this.tasksService.allTasks().filter((item)=>item.status === 'OPEN');
-            case 'in-progress':
-                return this.tasksService.allTasks().filter((item)=>item.status === 'IN_PROGRESS');
-            case 'done':
-                return this.tasksService.allTasks().filter((item)=>item.status === 'DONE');
-            default:
-                return this.tasksService.allTasks();
-        }
-    });
-
-
-    onChangeTasksFilter(filter: string) {
-        this.selectedFilter.set(filter);
+  private tasksService = inject(TasksServiceToken);
+  private selectedFilter = signal<string>("all");
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case "all":
+        return this.tasksService.allTasks();
+      case "open":
+        return this.tasksService
+          .allTasks()
+          .filter((item) => item.status === "OPEN");
+      case "in-progress":
+        return this.tasksService
+          .allTasks()
+          .filter((item) => item.status === "IN_PROGRESS");
+      case "done":
+        return this.tasksService
+          .allTasks()
+          .filter((item) => item.status === "DONE");
+      default:
+        return this.tasksService.allTasks();
     }
+  });
+
+  onChangeTasksFilter(filter: string) {
+    this.selectedFilter.set(filter);
+  }
 }
