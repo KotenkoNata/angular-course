7f3ce938f67b490f4a01d35c3327f837acddb5cc
KotenkoNata <kotenkonata8@gmail.com>
2025-05-02 16:19:53 +0200

    feat: Angular Has Multiple Injectors

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,29 +1,28 @@
 import { Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
 
-
-@Injectable({
-    providedIn: 'root'
-})
+// @Injectable({
+//     providedIn: 'root'
+// })
 export class TasksService {
-    private tasks = signal<Task[]>([]);
+  private tasks = signal<Task[]>([]);
 
-    allTasks = this.tasks.asReadonly();
+  allTasks = this.tasks.asReadonly();
 
-    addTask(taskData: { title: string, description: string }) {
-        const newTask = {
-            ...taskData,
-            id: Math.random().toString(),
-            status: 'OPEN' as TaskStatus,
-        };
-        this.tasks.update((oldTasks) => [...oldTasks, newTask]);
-    }
+  addTask(taskData: { title: string; description: string }) {
+    const newTask = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: "OPEN" as TaskStatus,
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
 
-    updateTaskStatus(taskId: string, newStatus: TaskStatus) {
-        this.tasks.update((oldTasks) =>
-            oldTasks.map((task) =>
-                task.id === taskId ? {...task, status: newStatus} : task
-            )
-        );
-    }
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+  }
 }
