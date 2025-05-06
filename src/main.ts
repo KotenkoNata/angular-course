84b11dc5615ed6286e89988fd3f7a50a6c8b66de
KotenkoNata <kotenkonata8@gmail.com>
2025-05-06 16:48:45 +0200

    feat: Setup Project

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,5 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { InjectionToken } from "@angular/core";
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from "./app/app.component";
-import { TasksService } from "./app/tasks/tasks.service";
+import { AppComponent } from './app/app.component';
 
-export const TasksServiceToken = new InjectionToken<TasksService>(
-  "task-service-token"
-);
-
-bootstrapApplication(AppComponent, {
-  providers: [{ provide: TasksServiceToken, useClass: TasksService }],
-}).catch((err) => console.error(err));
-
-// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
