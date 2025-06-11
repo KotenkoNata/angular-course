601563598ef7c4bf45b367079d36eef8d8b63e1f
KotenkoNata <kotenkonata8@gmail.com>
2025-06-11 16:09:36 +0200

    feat: Going Zoneless!

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, NgZone, OnInit, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit, signal } from "@angular/core";
 
 import { InfoMessageComponent } from "../info-message/info-message.component";
 
@@ -11,8 +11,7 @@ import { InfoMessageComponent } from "../info-message/info-message.component";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent implements OnInit {
-  private zone = inject(NgZone);
-  count = signal(0);
+  count = 0;
 
   get debugOutput() {
     console.log('[Counter] "debugOutput" binding re-evaluated.');
@@ -21,21 +20,21 @@ export class CounterComponent implements OnInit {
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.count.set(0);
+      this.count = 0;
     }, 4000);
 
-    this.zone.runOutsideAngular(() => {
       setTimeout(() => {
         console.log("Timer expired!");
       }, 5000);
-    });
   }
 
   onDecrement() {
-    this.count.update((prevCount) => prevCount - 1);
+    // this.count.update((prevCount) => prevCount - 1);
+    this.count = this.count - 1;
   }
 
   onIncrement() {
-    this.count.update((prevCount) => prevCount + 1);
+    // this.count.update((prevCount) => prevCount + 1);
+    this.count = this.count + 1;
   }
 }
