e978b5d6f8bcecba65ca316d0c5837d480cdd84e
KotenkoNata <kotenkonata8@gmail.com>
2025-06-11 16:44:01 +0200

    feat: Working with RxJS Operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, OnInit } from "@angular/core";
 
-import { interval } from 'rxjs';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +11,15 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit():void {
-    const subscription = interval(1000).subscribe({
+    const subscription = interval(1000).pipe(
+      map((val) => val*2)
+    ).subscribe({
       next: (value) => {
         console.log(`Interval tick: ${value}`);
       }
     });
 
     this.destroyRef.onDestroy(() => { 
-      console.log('Destroying subscription');
       subscription.unsubscribe();
      })
   }
