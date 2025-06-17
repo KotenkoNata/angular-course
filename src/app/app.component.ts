e651cbd2708160976bf57ac9cd45bb700d525a26
KotenkoNata <kotenkonata8@gmail.com>
2025-06-17 16:55:39 +0200

    feat: Converting Observables To Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from 
 
 import { interval, map } from 'rxjs';
 
-import { toObservable } from "@angular/core/rxjs-interop";
+import { toObservable, toSignal } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-root',
@@ -13,6 +13,8 @@ export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
   clickCount = signal(0);
   clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0});
   // interval = signal(0);
   // doubleInterval = computed(() => { 
   //   this.interval() * 2;
