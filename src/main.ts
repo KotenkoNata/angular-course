b6f1faf7977b159a1e95c2072f11bdfe613f40f8
KotenkoNata <kotenkonata8@gmail.com>
2025-06-27 16:30:02 +0200

    feat: Introducing HTTP Response Interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+  HttpEventType,
+} from "@angular/common/http";
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) { 
     console.log('Request made with ', request);
     // const req = request.clone({
     //     headers: request.headers.set('X-DEBUG', 'TESTING')
     // });
-    return next(request);
+    return next(request).pipe(
+        tap({
+            next: event => { 
+                if(event.type === HttpEventType.Response) {
+                    console.log('Response received', event.status);
+                    console.log("Response received", event.body);
+                }
+            }
+        })
+    );
 }
 
 
